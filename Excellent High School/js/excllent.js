932f130486363d923470dfdbb829d3c575623689
wisdomegba <wisdomegba@gmail.com>
2021-08-17 15:13:42 -0700

    Full School project

---
+++ Excellent High School/js/excllent.js
@@ -0,0 +1,53 @@
+// function ValidationEvent() {
+    // Storing Field Values In Variables
+    // var lname = document.getElementById("lname").value;
+    // var onames = document.getElementById("onames").value;
+    // var resad = document.getElementById("resad").value;
+    // var pad = document.getElementById("pad").value;
+    // var dob = document.getElementById("dob").value;
+    // var pob = document.getElementById("pob").value;
+    // var age = document.getElementById("age").value;
+    // var nat = document.getElementById("nat").value;
+    // var st = document.getElementById("st").value;
+    // var tc = document.getElementById("tc").value;
+    // var clas = document.getElementById("clas").value;
+    // var customRadioInline1 = document.getElementById("customRadioInline1").value;
+    // var zip = document.getElementById("zip").value;
+    // var dbt = document.getElementById("dbt").value;
+    // var tel = document.getElementById("tel").value;
+    // var email = document.getElementById("email").value;
+    // var psw = document.getElementById("psw").value;
+    // Regular Expression For Email
+    // var emailReg = /^([w-.]+@([w-]+.)+[w-]{2,4})?$/;
+    // Conditions
+  //   if (lname != '' && onames != '' && resad != '') {
+  //   if (pad != '' && dob != '' && pob != '') {
+  //   if (age != '' && nat != '' && st != '') {
+  //   if (tc != '' && clas != '' && customRadioInline1!= '') {
+  //   if (zip != '' && dbt != '' && tel != '') {
+  //   if (email.match(emailReg)) {
+  //   if (document.getElementById("male").checked || document.getElementById("female").checked) {
+  //   if (tel.length == 11) {
+  //   alert("All type of validation has done on OnSubmit event.");
+  //   return true;
+  //   } else {
+  //   alert("The tel No. must be at least 11 digit long!");
+  //   return false;
+  //   }
+  //   } else {
+  //   alert("You must select gender.....!");
+  //   return false;
+  //   }
+  //   } else {
+  //   alert("Invalid Email Address...!!!");
+  //   return false;
+  //   }
+  //   }else if(psw.length<8){  
+  //     alert("Password must be at least 6 characters long.");  
+  //     return false;  
+  //     } 
+  //   } else {
+  //     alert("All fields are required.....!");
+  //     return false;
+  //   }  
+  // }
